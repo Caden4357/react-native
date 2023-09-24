9386ee76dd377d23e7d949303b6db7be79252b6f
Caden Wilcox <wilcox4357@gmail.com>
2023-09-23 20:33:15 -0700

    got the basic of adding and displaying goals done

--- first-native-proj/RNFirstCourse/App.js
+++ first-native-proj/RNFirstCourse/App.js
@@ -1,32 +1,56 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-
 export default function App() {
-  return (
-    <View style={styles.mainContainer}>
+  const [goalText, setGoalText] = useState('');
+  const [goals, setGoals] = useState([]);
+  const handleGoalTextChange = (text) => {
+    setGoalText(text);
+  }
+  const handleAddGoal = () => {
+    setGoals(currentGoals => [...currentGoals, goalText]);
+    setGoalText('');
+  }
 
-      <View style={styles.flexContainer}>
-        <TextInput placeholder='Enter Your Goal' style={styles.textInput}/>
-        <Button title='Add Goal'/>
+  return (
+    <View style={styles.appContainer}>
+
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter Your Goal' style={styles.textInput} onChangeText={handleGoalTextChange} value={goalText}/>
+        <Button title='Add Goal' onPress={handleAddGoal}/>
       </View>
 
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List Of Goals</Text>
+        {goals?.map((goal, index) => <Text key={index}>{goal}</Text>)}
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  flexContainer: {
+  // ! Since views are flex containers and the flex direction is column by default, we can use flex to control the height of the child containers
+  appContainer: {
+    height: '100%',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom:24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderColor: '#cccccc',
     borderWidth: 1,
-    width: '80%',
-    padding: 8
+    padding: 8,
+    marginRight: 8,
+    width: '70%',
+  },  
+  goalsContainer: {
+    flex: 4,
   },
-  mainContainer: {
-    padding: 50
-  }
 });
