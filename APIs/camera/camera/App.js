ec7b36dd6f744f5a3c37bd20e201cf442a2fe010
Caden Wilcox <wilcox4357@gmail.com>
2023-10-16 21:56:59 -0700

    fixed bug (func comp cant be async) on photo capture now

--- APIs/camera/camera/App.js
+++ APIs/camera/camera/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { Camera } from 'react-native-vision-camera'
 import { useCameraPermission } from 'react-native-vision-camera'
@@ -20,9 +20,9 @@ const requestCameraPermission = async () => {
   }
 };
 function App() {
+  const camera = useRef(null)
   const device = useCameraDevice('back')
   const [camerIsOpen, setCameraIsOpen] = useState(false)
-
   // ! this would ask for permission immediately when the app starts
   // useEffect(() => {
   //   requestCameraPermission()
@@ -38,14 +38,23 @@ function App() {
     }
     )
   }
+  const takePhoto = async () => {
+    console.log('take photo');
+    const photo = await camera.current.takePhoto()
+    console.log(photo)
+  }
   return (
     <View>
       <Button title={camerIsOpen ? 'Close Camera' : 'Open Camera'} onPress={toggleCamera} />
       <Camera
-        style={{ width: '100%', height: '100%', display: camerIsOpen ? 'block' : 'none' }}
+        ref={camera}
+        style={{ width: '100%', height: '90%', display: camerIsOpen ? 'block' : 'none' }}
         device={device}
         isActive={camerIsOpen}
+        photo={true}
       />
+      <Button title='Take Photo' onPress={takePhoto} />
+      
     </View>
   )
 }
